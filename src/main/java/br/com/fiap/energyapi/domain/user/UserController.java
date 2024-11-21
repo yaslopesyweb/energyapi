9acb8c8ad3247e9599ad44558cdfd46b5c078b27
yaslopesyweb <slahyasmin@gmail.com>
2024-11-21 13:09:18 -0300

    Arrumando Users

--- src/main/java/br/com/fiap/energyapi/domain/user/UserController.java
+++ src/main/java/br/com/fiap/energyapi/domain/user/UserController.java
@@ -1,21 +1,30 @@
 package br.com.fiap.energyapi.domain.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);}
 
     @GetMapping("/{userId}")
     public Optional<User> getUserById(@PathVariable String userId) {
